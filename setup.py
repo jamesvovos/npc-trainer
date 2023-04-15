d0466a1bd73bbcfac9b45a9fd78dd3a8a294bbfd
jamesvovos <james.vovos@gmail.com>
2023-04-15 18:51:17 +1000

    fixed project

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = "A package that allows you to build your own NPC AI models"
 
 # Setting up
 setup(
-    name="npc-trainer",
+    name="myproject",
     version=VERSION,
     author="James Vovos",
     author_email="<james.vovos@gmail.com>",
@@ -21,7 +21,6 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["spaCy", "pytorch"],
     keywords=["python", "ai", "pytorch", "npc", "training", "spaCy"],
     classifiers=[
         "Development Status :: 1 - Planning",
